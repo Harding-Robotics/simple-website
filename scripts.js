777f0a3230a1c8e30b3c41cf9942ce2dbeba0edf
Patrick Levy <mrpatricklevy@gmail.com>
2019-12-14 15:16:22 -0600

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+// Start of execution
+$(document).ready( function(){
+  // wait for document to be loaded
+  console.log('document ready');
+  hello()
+});
+
+
+function hello() {
+  // this will appear in the browser dev tools
+  console.log('hi')
+}
